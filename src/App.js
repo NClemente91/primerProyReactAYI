3bdc1a80dabd1ed97980eb8e80b87413c46b9d31
NClemente91 <nicoclemente91@gmail.com>
2022-07-14 10:32:12 -0300

    Primer Commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+//import logo from './logo.svg';
+import './App.css';
+import Carrito from './Carrito';
+import NuevoComponente from './NuevoComponente';
+import Producto from './Producto'
+
+function App() {
+  return (
+    <div className="App">
+      <NuevoComponente />
+      <Carrito />
+      <Producto />
+      <Producto />
+      <Producto />
+      <Producto />
+      <Producto />
+      <Producto />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Producto.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Pruducto() {
+    return(
+        <div>
+            Producto X
+        </div>
+    )
+}
+
+export default Pruducto
